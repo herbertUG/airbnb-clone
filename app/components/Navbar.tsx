7d00a8ad6dc429bb2eb1c4eb26637fc7945ad560
herbertug <herbertoketayot@gmail.com>
2024-04-24 13:45:09 +0300

    add navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+import DesktopLogo from "../../public/airbnb-desktop.png";
+import MobileLogo from "../../public/airbnb-mobile.webp";
+import { UserNav } from "./UserNav";
+
+export function Navbar() {
+  return (
+    <nav className="w-full border-b">
+      <div className="flex items-center justify-between container mx-auto px-5 lg:px-10 py-5">
+        <Link href="/">
+          <Image
+            src={DesktopLogo}
+            alt="Desktop logo"
+            className="w-32 hidden lg:block"
+          />
+          <Image
+            src={MobileLogo}
+            alt="Mobile logo"
+            className="w-12 lg:hidden block"
+          />
+        </Link>
+
+        <div className="rounded-full border px-5 py-2">
+          <h1>Hello from search</h1>
+        </div>
+
+        <UserNav/>
+
+      </div>
+    </nav>
+  );
+}
---
+++ app/components/UserNav.tsx
@@ -0,0 +1,28 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MenuIcon } from "lucide-react";
+
+export function UserNav() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <div className="rounded-full border px-2 py-2 lg:px-4 lg:py-2 flex items-center gap-x-3">
+          <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5 " />
+          <img
+            src="https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"
+            alt="Image of the user"
+            className="rounded-full h-8 w-8 hidden lg:block"
+          />
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-[200px]">
+        <DropdownMenuItem>Register</DropdownMenuItem>
+        <DropdownMenuItem>Login</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
